00e1e4a527242cea6d2a4a631c38c898b015afec
Sidney Mau <sidneymau@gmail.com>
2024-09-25 07:10:52 -0700

    rename plan to execplan

--- src/dataplan/dataplan.py
+++ src/dataplan/dataplan.py
@@ -21,18 +21,18 @@ class DataPlan:
     A plan to process data
     """
 
-    def __init__(self, plan, source=None):
+    def __init__(self, execplan, source=None):
         """
-        Construct a DataPlan from an existing plan
+        Construct a DataPlan from an exec plan
         """
-        self._plan = plan
+        self._execplan = execplan
         if isinstance(source, list):
             self._source = source
         else:
             self._source = [source]
 
     def __repr__(self):
-        plan_repr = str(self.plan)
+        execplan_repr = str(self.execplan)
         if self.source is not None:
             source_repr = "\n  ".join([
                 f"{type(source)} at {hex(id(source))}"
@@ -43,11 +43,18 @@ class DataPlan:
         return (
             f"DataPlan\n"
             f"--------------------------------------------------------------------------------\n"
-            f"{plan_repr}\n"
+            f"{execplan_repr}\n"
             f"--------------------------------------------------------------------------------\n"
             f"Sources:\n  {source_repr}\n"
             f"--------------------------------------------------------------------------------"
         )
+
+    @property
+    def execplan(self):
+        """
+        The current plan to be executed.
+        """
+        return self._execplan
 
     @classmethod
     def from_dict(cls, mapping, **kwargs):
@@ -93,13 +100,6 @@ class DataPlan:
         return cls(_plan, source=dataset)
 
     @property
-    def plan(self):
-        """
-        The current plan to be executed.
-        """
-        return self._plan
-
-    @property
     def source(self):
         """
         The source over which the plan will operate.
@@ -111,21 +111,21 @@ class DataPlan:
         Apply a projection (e.g., select columns, rename columns, construct new
         columns, etc.).
         """
-        _project = declare_project(self.plan, *args, **kwargs)
+        _project = declare_project(self.execplan, *args, **kwargs)
         return self.__class__(_project, source=self.source)
 
     def filter(self, *args, **kwargs):
         """
         Apply a filter.
         """
-        _filter = declare_filter(self.plan, *args, **kwargs)
+        _filter = declare_filter(self.execplan, *args, **kwargs)
         return self.__class__(_filter, source=self.source)
 
     def join(self, other, *args, **kwargs):
         """
         Perform a join with another DataPlan.
         """
-        _join = declare_join(self.plan, other.plan, *args, **kwargs)
+        _join = declare_join(self.execplan, other.execplan, *args, **kwargs)
         _source = [self.source, other.source]
         source = list(
             functools.reduce(
@@ -140,7 +140,7 @@ class DataPlan:
         """
         Perform an asof join with another DataPlan.
         """
-        _join = declare_join_asof(self.plan, other.plan, *args, **kwargs)
+        _join = declare_join_asof(self.execplan, other.execplan, *args, **kwargs)
         _source = [self.source, other.source]
         source = list(
             functools.reduce(
@@ -155,27 +155,27 @@ class DataPlan:
         """
         Apply an ordering.
         """
-        _order_by = declare_order_by(self.plan, *args, **kwargs)
+        _order_by = declare_order_by(self.execplan, *args, **kwargs)
         return self.__class__(_order_by, source=self.source)
 
     def aggregate(self, *args, **kwargs):
         """
         Perform a series of aggregations.
         """
-        _aggregate = declare_aggregate(self.plan, *args, **kwargs)
+        _aggregate = declare_aggregate(self.execplan, *args, **kwargs)
         return self.__class__(_aggregate, source=self.source)
 
     def _to_reader(self, use_threads=True):
         """
         Execute the plan lazily as a reader.
         """
-        return self.plan.to_reader(use_threads=use_threads)
+        return self.execplan.to_reader(use_threads=use_threads)
 
     def _to_table(self, use_threads=True):
         """
         Execute the plan and return a pyarrow Table.
         """
-        return self.plan.to_table(use_threads=use_threads)
+        return self.execplan.to_table(use_threads=use_threads)
 
     def to_reader(self, use_threads=True):
         """
